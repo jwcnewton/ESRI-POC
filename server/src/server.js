14b3600dde67f72baf7c7fc78c8992add964a704
jwcnewton <jwcnewton@gmail.com>
2021-01-10 13:32:10 +0000

    update and fix cors issues

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-
+const cors = require('cors')
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
